669c8c460ef78f6b5f5d1d28af7f8c10df4d0513
Kevin <kevin_ni1992@outlook.com>
2015-10-26 11:45:19 +0800

    新增数据库帮助类，以及各表对应的实现类

---
+++ app/src/main/java/com/fengyunweather/app/model/Province.java
@@ -0,0 +1,29 @@
+package com.fengyunweather.app.model;
+
+/**
+ * Created by Kevin on 2015/10/26.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getProvinceName(){
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName){
+        this.provinceName=provinceName;
+    }
+    public String getProvinceCode(){
+        return provinceCode;
+    }
+    public void setProvinceCode(String provinceCode){
+        this.provinceCode=provinceCode;
+    }
+}
